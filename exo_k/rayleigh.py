jovian-explorer/exo_k

commit 2ee1e501643496d7bba1151783975d7bd5a33f3b

Corrected bugs in Gas_mix. logp arrays convertd to float in many places to solve problems with powers with negative integer exponents. Solved a bug in rayleigh when no molecule known. Changed function name xsec_to_ktable to xtable_to_ktable.

exo_k/rayleigh.py
@@ -49,7 +49,7 @@ class Rayleigh(Singleton):
             if mol is 'He':
                 tmp=((5.484E-14)*(wave**(-4.))*(1+(2.44E5)*(wave**(-2.))))*1E-4
             else:
-                pass
+                tmp=np.zeros(wave.size)
             res+=x*tmp
 
         return res

exo_k/atm.py
@@ -322,7 +322,10 @@ class Atm(Atm_profile):
 
         See :any:`gas_mix.Gas_mix.cross_section` for details.
         """
-        self.Nw, self.wns, self.wnedges, self.kdata = self.gas_mix.cross_section(**kwargs)
+        self.kdata = self.gas_mix.cross_section(**kwargs)
+        self.Nw=self.gas_mix.Nw
+        self.wns=self.gas_mix.wns
+        self.wnedges=self.gas_mix.wnedges
 
     def emission_spectrum(self, integral=True, mu0=0.5, **kwargs):
         """Computes the emission flux at the top of the atmosphere (in W/m^2/cm^-1)

exo_k/data_table.py
@@ -182,9 +182,9 @@ class Data_table(object):
                 Whether the interpolation is linear in kdata or in log(kdata)
         """
         if hasattr(logp_array, "__len__"):
-            logp_array=np.array(logp_array)
+            logp_array=np.array(logp_array, dtype=float)
         else:
-            logp_array=np.array([logp_array])
+            logp_array=np.array([logp_array], dtype=float)
         if hasattr(t_array, "__len__"):
             t_array=np.array(t_array)
         else:
@@ -245,7 +245,7 @@ class Data_table(object):
         """
         if x_array is not None: print('be careful, providing an x_array is usually for Ktable5d')
         t_array=np.array(t_array)
-        logp_array=np.array(logp_array)
+        logp_array=np.array(logp_array, dtype=float)
         tind,tweight=interp_ind_weights(t_array,self.tgrid)
         lpind,lpweight=interp_ind_weights(logp_array,self.logpgrid)
         lpindextended=lpind[:,None]
@@ -545,9 +545,9 @@ class Data_table(object):
         """
         if x_array is not None: print('be careful, providing an x_array is usually for Ktable5d')
         if hasattr(logp_array, "__len__"):
-            logp_array=np.array(logp_array)
+            logp_array=np.array(logp_array, dtype=float)
         else:
-            logp_array=np.array([logp_array])
+            logp_array=np.array([logp_array], dtype=float)
         if hasattr(t_array, "__len__"):
             t_array=np.array(t_array)
         else:

exo_k/gas_mix.py
@@ -14,19 +14,24 @@ class Gas_mix(object):
     can be used to compute the opacity of the gas
     """
 
-    def __init__(self, composition, bg_gas=None, logp_array=None, t_array=None,
+    def __init__(self, composition={}, bg_gas=None, logp_array=None, t_array=None,
         k_database=None, cia_database=None):
         """__init_ Instantiates
         a Gas_mix object and computes the vmr of the 'background' gas.
         """
+        self.Narray=None
+        self._wn_range=None
+        self.iw_min=None
+        self.iw_max=None
+        self.wns=None
+        self.wnedges=None
+        self.Nw=None
+
         self.set_composition(composition, bg_gas=bg_gas)
         self.set_logPT(logp_array=logp_array, t_array=t_array)
         self.set_k_database(k_database)
         self.set_cia_database(cia_database)
 
-        self._wn_range=None
-        self.iw_min=None
-        self.iw_max=None
 
     def set_composition(self, composition, bg_gas=None):
         """Reset composition and computes the vmr of the 'background' gas.
@@ -53,14 +58,20 @@ class Gas_mix(object):
         """Sets the pressure (in Pa) and temperature fields.
         """
         if logp_array is not None:
-            self.logp_array=np.array(logp_array)
-            self.t_array=np.array(t_array)
+            if hasattr(logp_array, "__len__"):
+                self.logp_array=np.array(logp_array, dtype=float)
+            else:
+                self.logp_array=np.array([logp_array], dtype=float)
+
+            if hasattr(t_array, "__len__"):
+                self.t_array=np.array(t_array)
+            else:
+                self.t_array=np.array([t_array])
+
             if self.logp_array.size != self.t_array.size:
                 raise TypeError(\
                     'logp_array and t_array should be 1d lists or arrays of the same size')
             self.Narray=self.logp_array.size
-        else:
-            self.Narray=None
 
     def get_background_vmr(self):
         """Computes the volume mixing ratio of the background gas in a mix
@@ -220,7 +231,8 @@ class Gas_mix(object):
                 & (self.kdatabase.wnedges <= self._wn_range[1]))[0][[0,-1]]
             # to be consistent with interpolate_kdata
 
-    def cross_section(self, wl_range=None, wn_range=None, rayleigh=True,
+    def cross_section(self, composition=None, logp_array=None, t_array=None,
+            wl_range=None, wn_range=None, rayleigh=True,
             write=0, random_overlap=False, **kwargs):
         """Computes the cross section (m^2/total number of molecule) for the mix
         at each of the logPT points as a function of wavenumber (and possibly g point).
@@ -239,14 +251,15 @@ class Gas_mix(object):
 
         Returns
         -------
-            Nw: int
-                Number of wavenumber bins.
-            wns: array
-                Wavenumber array.
-            wnedges: array
-                Wavenumber of the edges of the bins.
             kdata_array: array
                 Cross section array of shape (layer number, Nw (, Ng if corrk)).
+
+        After every computation, the following variables are updated to account for any possible
+        change in spectral range:
+
+          * self.Nw, Number of wavenumber bins
+          * self.wns, Wavenumber array
+          * self.wnedges, Wavenumber of the edges of the bins
         """
         if self.kdatabase is None: raise RuntimeError("""kdatabase not provided. 
         Use the kdatabase keyword during initialization or use the set_database method.""")
@@ -257,9 +270,13 @@ class Gas_mix(object):
             (not np.array_equal(self.cia_database.wns,self.kdatabase.wns)):
             raise RuntimeError("""CIAdatabase not sampled on the right wavenumber grid.
             You should probably run something like CIAdatabase.sample(Kdatabase.wns).""")
+        
+        self.set_logPT(logp_array=logp_array, t_array=t_array)
         if self.Narray is None:
             raise RuntimeError('You must prescribe logP (in Pa) and T arrays first.')
 
+        if composition is not None:
+            self.set_composition(composition)
         molecs=self.composition.keys()
         mol_to_be_done=list(set(molecs).intersection(self.kdatabase.molecules))
         if all(elem in self.kdatabase.molecules for elem in molecs):
@@ -272,9 +289,9 @@ class Gas_mix(object):
 
         self.set_spectral_range(wl_range=wl_range, wn_range=wn_range)
         self.compute_wn_range_indices()
-        wnedges=np.copy(self.kdatabase.wnedges[self.iw_min:self.iw_max+1])
-        wns=np.copy(self.kdatabase.wns[self.iw_min:self.iw_max])
-        Nw=wns.size
+        self.wnedges=np.copy(self.kdatabase.wnedges[self.iw_min:self.iw_max+1])
+        self.wns=np.copy(self.kdatabase.wns[self.iw_min:self.iw_max])
+        self.Nw=self.wns.size
 
         vmr_prof, cst_prof=self.get_vmr_array((self.Narray,))
         first_mol=True
@@ -288,28 +305,28 @@ class Gas_mix(object):
             else:
                 if random_overlap and (self.kdatabase.Ng is not None):
                     kdata_array=RandOverlap_2_kdata_prof(self.Narray,
-                        Nw,self.kdatabase.Ng, 
-                        kdata_array,tmp_kdata,self.kdatabase.weights,
+                        self.Nw, self.kdatabase.Ng, 
+                        kdata_array,tmp_kdata, self.kdatabase.weights,
                         self.kdatabase.ggrid)
                 else:
                     kdata_array+=tmp_kdata
 
         if rayleigh or (self.cia_database is not None):
-            cont_sig=np.zeros((self.Narray,Nw))
+            cont_sig=np.zeros((self.Narray,self.Nw))
             if self.cia_database is not None:
                 cont_sig+=self.cia_database.cia_cross_section(self.logp_array,
                     self.t_array, vmr_prof, wngrid_limit=self._wn_range)
             if rayleigh:
                 if cst_prof:
-                    cont_sig+=Rayleigh().sigma(wns, self.composition)
+                    cont_sig+=Rayleigh().sigma(self.wns, self.composition)
                 else:
-                    cont_sig+=Rayleigh().sigma_array(wns, vmr_prof)
+                    cont_sig+=Rayleigh().sigma_array(self.wns, vmr_prof)
             if self.kdatabase.Ng is None:
                 kdata_array+=cont_sig
             else:
                 kdata_array+=cont_sig[:,:,None]
 
-        return Nw, wns, wnedges, kdata_array
+        return kdata_array
     
     def __getitem__(self, molecule):
         """Overrides getitem
@@ -363,6 +380,18 @@ class Gas_mix(object):
         res._wn_range=np.copy(self._wn_range)
         res.iw_min=self.iw_min
         res.iw_max=self.iw_max
+
+    @property
+    def wls(self):
+        """Returns the wavelength array for the bin centers
+        """
+        if self.wns is not None: return 10000./self.wns
+
+    @property
+    def wledges(self):
+        """Returns the wavelength array for the bin edges
+        """
+        if self.wnedges is not None: return 10000./self.wnedges
 
     def mix_with(self, other_gas, vmr_other_gas):
         """Mix with other Gas_mix.

exo_k/kdatabase.py
@@ -204,7 +204,7 @@ class Kdatabase(object):
             You should probably use convert_p_unit().""")
         for mol in self.molecules:
             self.ktables[mol].remap_logPT(logp_array=logp_array,t_array=t_array)
-        self.logpgrid=np.array(logp_array)
+        self.logpgrid=np.array(logp_array, dtype=float)
         self.pgrid   =10**self.logpgrid
         self.tgrid   =np.array(t_array)
         self.Np      =self.logpgrid.size

exo_k/ktable.py
@@ -24,7 +24,7 @@ class Ktable(Data_table):
     Based on the Data_table class that handles basic operations common to Xtable.
     """
         
-    def __init__(self, *filename_filters, filename=None, xsec=None, path=None,
+    def __init__(self, *filename_filters, filename=None, xtable=None, path=None,
         p_unit='unspecified', file_p_unit='unspecified',
         kdata_unit='unspecified', file_kdata_unit='unspecified',
         remove_zeros=False, search_path=None, mol=None,
@@ -43,8 +43,8 @@ class Ktable(Data_table):
                 This path will be searched for a file
                 with all the filename_filters in the name.
                 The filename_filters can contain '*'.
-            xsec: Xtable object
-                If no filename nor filename_filters are provided, this xsec object will be used to
+            xtable: Xtable object
+                If no filename nor filename_filters are provided, this xtable object will be used to
                 create a ktable. In this case, wavenumber bins must be given
                 with the wnedges keyword.
             path: str
@@ -106,8 +106,8 @@ class Ktable(Data_table):
             else:
                 raise NotImplementedError("""Requested format not recognized.
             Should end with .pickle, .hdf5, or .h5""")
-        elif xsec is not None:
-            self.xsec_to_ktable(xsec=xsec, **kwargs)
+        elif xtable is not None:
+            self.xtable_to_ktable(xtable=xtable, **kwargs)
         elif path is not None:
             self.read_LMDZ(path=path, mol=mol, **kwargs)
         else:                  #if there is no input file, just create an empty object 
@@ -386,7 +386,7 @@ class Ktable(Data_table):
                 np.zeros(self.Np*self.Nt*self.Nw)).reshape((1,self.Np*self.Nt*self.Nw*(self.Ng+1)))
             np.savetxt(os.path.join(dirname,'corrk_gcm_'+band+'.dat'),data_to_write,fmt=fmt)
 
-    def xsec_to_ktable(self, xsec=None, wnedges=None, weights=None, ggrid=None,
+    def xtable_to_ktable(self, xtable=None, wnedges=None, weights=None, ggrid=None,
         quad='legendre', order=20, mid_dw=True, write=0):
         """Fills the :class:`~exo_k.ktable.Ktable` object with a k-coeff table computed
         from a :class:`~exo_k.xtable.Xtable` object.
@@ -395,7 +395,7 @@ class Ktable(Data_table):
 
         Parameters
         ----------
-            xsec: :class:`~exo_k.xtable.Xtable`
+            xtable: :class:`~exo_k.xtable.Xtable`
                 input Xtable object instance
             wnedges: Array
                 edges of the wavenumber bins to be used to compute the corrk
@@ -410,17 +410,17 @@ class Ktable(Data_table):
                 Order of the Gauss legendre quadrature used. Default is 20.
             mid_dw: boolean, optional
 
-                * If True, the Xsec values in the high resolution xsec data are assumed to
+                * If True, the Xsec values in the high resolution xtable data are assumed to
                   cover a spectral interval that is centered around
                   the corresponding wavenumber value.
                   The first and last Xsec values are discarded. 
                 * If False, each interval runs from the wavenumber value to the next one.
                   The last Xsec value is dicarded.
         """        
-        if xsec is None: raise TypeError("You should provide an input Xtable object")
+        if xtable is None: raise TypeError("You should provide an input Xtable object")
         if wnedges is None: raise TypeError("You should provide an input wavenumber array")
 
-        self.copy_attr(xsec,cp_kdata=False)
+        self.copy_attr(xtable,cp_kdata=False)
         self.wnedges=np.array(wnedges)
         if self.wnedges.size<2: raise TypeError('wnedges should at least have two values')
         self.wns=0.5*(self.wnedges[1:]+self.wnedges[:-1])
@@ -443,17 +443,17 @@ class Ktable(Data_table):
         self.Nw=self.wns.size
         self.kdata=np.zeros(self.shape)
         if mid_dw:
-            dwn_hr=(xsec.wns[2:]-xsec.wns[:-2])*0.5
-            wn_hr=xsec.wns[1:-1]
+            dwn_hr=(xtable.wns[2:]-xtable.wns[:-2])*0.5
+            wn_hr=xtable.wns[1:-1]
         else:
-            dwn_hr=(xsec.wns[1:]-xsec.wns[:-1])
-            wn_hr=xsec.wns[:-1]
+            dwn_hr=(xtable.wns[1:]-xtable.wns[:-1])
+            wn_hr=xtable.wns[:-1]
         for iP in range(self.Np):
           for iT in range(self.Nt):
             if mid_dw:
-                k_hr=xsec.kdata[iP,iT,1:-1]
+                k_hr=xtable.kdata[iP,iT,1:-1]
             else:
-                k_hr=xsec.kdata[iP,iT,:-1]
+                k_hr=xtable.kdata[iP,iT,:-1]
             #print(self.ggrid)
             #print(wn_hr[[0,-1]])
             #print(self.wnedges[[0,-1]])
@@ -547,7 +547,7 @@ class Ktable(Data_table):
         self.Ng=self.weights.size
 
         conversion_factor=u.Unit(grid_p_unit).to(u.Unit('Pa'))
-        self.logpgrid=np.array(logpgrid)+np.log10(conversion_factor)
+        self.logpgrid=np.array(logpgrid, dtype=float)+np.log10(conversion_factor)
         self.pgrid=10**self.logpgrid #in Pa
         self.p_unit='Pa'
         self.Np=self.logpgrid.size

exo_k/ktable5d.py
@@ -314,7 +314,7 @@ class Ktable5d(Data_table):
         self.Ng=self.weights.size
 
         conversion_factor=u.Unit(grid_p_unit).to(u.Unit('Pa'))
-        self.logpgrid=np.array(logpgrid)+np.log10(conversion_factor)
+        self.logpgrid=np.array(logpgrid, dtype=float)+np.log10(conversion_factor)
         self.pgrid=10**self.logpgrid #in Pa
         self.p_unit='Pa'
         self.Np=self.logpgrid.size

exo_k/xtable.py
@@ -268,7 +268,7 @@ class Xtable(Data_table):
             self.mol=os.path.basename(self.filename).split(self._settings._delimiter)[0]
 
         conversion_factor=u.Unit(grid_p_unit).to(u.Unit('Pa'))
-        self.logpgrid=np.array(logpgrid)+np.log10(conversion_factor)
+        self.logpgrid=np.array(logpgrid, dtype=float)+np.log10(conversion_factor)
         self.pgrid=10**self.logpgrid #in Pa
         self.p_unit='Pa'
         self.Np=self.logpgrid.size

commit 0f4d21747a07322ccb53cd017da748a2d171e302

Corrects a bug in Rayleigh introduced in 2ee1e501643496d7bba1151783975d7bd5a33f3b

exo_k/rayleigh.py
@@ -42,15 +42,16 @@ class Rayleigh(Singleton):
         res=np.zeros(wns.size)
         wave = 1e8/wns
         for mol, x in vmr.items():
-            if mol is 'H2':
+            to_add=True
+            if mol == 'H2':
                 tmp=((8.14E-13)*(wave**(-4.))* \
                     (1+(1.572E6)*(wave**(-2.))+(1.981E12)*(wave**(-4.))))*1E-4
                 #res+=x*(8.14e-33+1.28e-42*wns**2+1.61e-51*wns**4)*wns**4/1.e16
-            if mol is 'He':
+            elif mol == 'He':
                 tmp=((5.484E-14)*(wave**(-4.))*(1+(2.44E5)*(wave**(-2.))))*1E-4
             else:
-                tmp=np.zeros(wave.size)
-            res+=x*tmp
+                to_add=False
+            if to_add: res+=x*tmp
 
         return res
 
@@ -78,11 +79,11 @@ class Rayleigh(Singleton):
             if first_mol:
                 res=np.zeros((x_array.size,wns.size))
                 first_mol=False
-            if mol is 'H2':
+            if mol == 'H2':
                 tmp=((8.14E-13)*(wave**(-4.))* \
                     (1+(1.572E6)*(wave**(-2.))+(1.981E12)*(wave**(-4.))))*1E-4
                 #res+=x*(8.14e-33+1.28e-42*wns**2+1.61e-51*wns**4)*wns**4/1.e16
-            if mol is 'He':
+            elif mol == 'He':
                 tmp=((5.484E-14)*(wave**(-4.))*(1+(2.44E5)*(wave**(-2.))))*1E-4
             else:
                 to_add=False